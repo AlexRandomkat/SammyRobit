9cfce5d1f94a19e71c9a71b104bf116a6f677e15
jonnyboy6 <jonathan.narita@rocklinusd.org>
2017-11-22 16:38:32 -0800

    camera
    now has camera features woooooooooo

--- Sammy/src/org/usfirst/frc/team6644/robot/Robot.java
+++ Sammy/src/org/usfirst/frc/team6644/robot/Robot.java
@@ -1,6 +1,12 @@
 package org.usfirst.frc.team6644.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.CameraServer;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -52,6 +58,26 @@ public class Robot extends IterativeRobot {
 		System.out.println("Twist axis channel: "+joystick.getAxisChannel(Joystick.AxisType.kTwist));
 		System.out.println("Throttle axis channel: "+joystick.getAxisChannel(Joystick.AxisType.kThrottle));
 		//end test stuff
+		//start camera stuff
+		//CameraServer.getInstance().startAutomaticCapture();//simple camera stuff
+		new Thread(() -> {
+			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+			camera.setResolution(640, 480);
+
+			CvSink cvSink = CameraServer.getInstance().getVideo();
+			CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+
+			Mat source = new Mat();
+			Mat output = new Mat();
+
+			while (!Thread.interrupted()) {
+				cvSink.grabFrame(source);
+				Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+				outputStream.putFrame(output);
+			}
+		}).start();
+	//advanced camera stuff
+	 //end camera stuff
 	}
 
 	/**
