16e75ddf6c6b17a4588ff8bf51507d9714e5011c
adumbidiot <nathaniel.daniel23@outlook.com>
2018-01-08 14:17:57 -0800

    IR Avoidance Program

---
+++ Sammy/src/org/usfirst/frc/team6644/robot/commandGroups/AvoidAutonomous.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team6644.robot.commandGroups;
+
+import org.usfirst.frc.team6644.robot.commands.AutonomousTurn;
+import org.usfirst.frc.team6644.robot.commands.IRStop;
+import org.usfirst.frc.team6644.robot.commands.Turn;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AvoidAutonomous extends CommandGroup {
+	public AvoidAutonomous(double speed) {
+		for (int i = 0; i != 1000; i++) {
+			avoidLoop(speed);
+		}
+	}
+
+	public void avoidLoop(double speed) {
+		addSequential(new IRStop());
+		addSequential(new Turn(speed, 90));
+	}
+}
